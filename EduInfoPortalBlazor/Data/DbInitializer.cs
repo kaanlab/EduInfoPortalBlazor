9e3bdbbddfcd5f8428635f921d1777de7eff3471
kaanlab <kaan@live.ru>
2021-01-05 13:37:53 +0300

    Initial commit

---
+++ EduInfoPortalBlazor/Data/DbInitializer.cs
@@ -0,0 +1,30 @@
+﻿using EduInfoPortalBlazor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EduInfoPortalBlazor.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(EduInfoDbContext context)
+        {
+            context.Database.EnsureCreated();
+            if (context.Institutions.Any())
+            {
+                return;
+            }
+
+            var institutions = new Institution[]
+            {
+                new Institution { Name ="Петрозаводское президентское кадетсое училище",Address ="185000, республика Карелия, город Петрозаводск, Комсомольский проспект, дом 11"},
+                new Institution { Name = "Оренбургское президентское кадетское училище", Address = "460010, Оренбургская область, г.Оренбург, ул.Пушкинская, д.63" },
+                new Institution { Name = "Ставропольское президентское кадетское училище", Address = "355017, Ставропольский край, г.Ставрополь, ул. Ленина, д.320" }
+            };
+
+            context.Institutions.AddRange(institutions);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# EduInfoPortalBlazor
+ \EduInfoPortalBlazor
